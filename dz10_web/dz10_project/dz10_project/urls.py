c7cf7993ceb99d7311e06a0356e43a40ba4b8835
nickskywolf <nickskywolf@gmail.com>
2024-01-07 14:37:09 +0200

    update version

--- dz10_web/dz10_project/dz10_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("quotes.urls")),
-    # path("users/", include("users.urls"))
-]
